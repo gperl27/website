dd1fe2c6475dcebed2b55883c4c3b341c743f1db
gperl27 <gperlman27@gmail.com>
2019-05-02 09:26:07 -0400

    add contact form functionality

--- src/pages/form.tsx
+++ src/pages/form.tsx
@@ -1,7 +1,13 @@
 import { PageRendererProps } from "gatsby"
+import Slider from "rc-slider"
+// tslint:disable-next-line:no-submodule-imports
+import "rc-slider/assets/index.css"
 import React, { ChangeEvent, useState } from "react"
-import styled from "styled-components"
+import styled, { css } from "styled-components"
 import { Layout } from "../components/layout"
+
+const createSliderWithTooltip = Slider.createSliderWithTooltip
+const Range = createSliderWithTooltip(Slider.Range)
 
 type Props = PageRendererProps
 
@@ -11,7 +17,7 @@ const encode = (data: { [key: string]: any }) => {
     .join("&")
 }
 
-const StyledInput = styled.input`
+const BaseInputCss = css`
   box-shadow: inset 0 1px 2px rgba(10, 10, 10, 0.1);
   border-radius: 4px;
   background-color: #fff;
@@ -19,7 +25,6 @@ const StyledInput = styled.input`
   align-items: center;
   border: 1px solid #dbdbdb;
   display: inline-flex;
-  height: 2.25em;
   justify-content: flex-start;
   line-height: 1.5;
   padding: calc(0.375em - 1px) calc(0.625em - 1px);
@@ -41,31 +46,118 @@ const StyledInput = styled.input`
   }
 `
 
+const StyledInput = styled.input`
+  height: 2.25em;
+  ${BaseInputCss};
+`
+
+const StyledTextArea = styled.textarea`
+  ${BaseInputCss};
+  resize: none;
+`
+
 const InputWrapper = styled.div`
   box-sizing: border-box;
   clear: both;
-  font-size: 1rem;
   position: relative;
   text-align: left;
 `
 
+const BaseFormContainer = styled.div`
+  display: flex;
+`
+
+const BaseFormColumn = styled.div``
+
+const LeftBaseFormContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  padding-right: 1rem;
+`
+
+const RightBaseFormContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  padding-left: 1rem;
+  text-align: center;
+`
+
+const AboutButtons = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+`
+
+const SliderWrapper = styled.div`
+  margin-bottom: 2rem;
+`
+
+const SliderLabel = styled.p`
+  margin-bottom: 3rem;
+`
+
+const SalarySlider = styled(SliderWrapper)`
+  margin-top: 1rem;
+`
+
+const DEFAULT_SALARY_RANGE = [120, 140]
+
+type PersonType = "recruiter" | "entrepreneur"
+
+const Button = props => {
+  return (
+    <button type="button" {...props}>
+      {props.children}
+    </button>
+  )
+}
+
 export const Form = (props: Props) => {
-  const [isRecruiter, setIsRecruiter] = useState(false)
+  const [personType, setIsPersonType] = useState<PersonType | undefined>(
+    undefined
+  )
   const [fields, setFields] = useState({})
 
   const handleChange = (
     e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
   ) => {
-    setFields({ [e.target.name]: e.target.value })
+    setFields({
+      ...fields,
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  const onSalaryChange = (values: [number, number]) => {
+    setFields({
+      ...fields,
+      salary: values,
+    })
+  }
+
+  const onBudgetChange = (values: [number, number]) => {
+    setFields({
+      ...fields,
+      budget: values,
+    })
+  }
+
+  const onTimelineChange = (values: [number, number]) => {
+    setFields({
+      ...fields,
+      timeline: values,
+    })
   }
 
   const handleSubmit = (e: ChangeEvent<HTMLFormElement>) => {
+    console.log(fields, "fields")
     e.preventDefault()
     const form = e.target
     fetch("/", {
       body: encode({
         "form-name": form.getAttribute("name"),
         ...fields,
+        personType,
       }),
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
       method: "POST",
@@ -74,15 +166,113 @@ export const Form = (props: Props) => {
       .catch(error => alert(error))
   }
 
-  const SalaryInput = props => (
-    <InputMask {...props} mask="+4\9 99 999 99" maskChar=" " />
-  )
+  const renderJobForm = () => {
+    return (
+      <>
+        <h3>Interesting Job Description</h3>
+        <InputWrapper>
+          <label>Company Name</label>
+          <StyledInput
+            required={true}
+            type="text"
+            name="company"
+            onChange={handleChange}
+          />
+        </InputWrapper>
+        <InputWrapper>
+          <label>Location</label>
+          <StyledInput
+            placeholder={'You can put "Remote" here'}
+            required={true}
+            type="text"
+            name="location"
+            onChange={handleChange}
+          />
+        </InputWrapper>
+        <InputWrapper>
+          <label>Job Title</label>
+          <StyledInput
+            required={true}
+            type="text"
+            name="location"
+            onChange={handleChange}
+          />
+        </InputWrapper>
+        <SalarySlider>
+          <SliderLabel>Salary</SliderLabel>
+          <Range
+            allowCross={false}
+            min={100}
+            max={200}
+            defaultValue={DEFAULT_SALARY_RANGE}
+            tipFormatter={(value: string) => `$${value}k`}
+            tipProps={{ visible: true }}
+            step={5}
+            onChange={onSalaryChange}
+          />
+        </SalarySlider>
+        <InputWrapper>
+          <label> What is the gig all about?</label>
+          <StyledTextArea
+            placeholder={
+              "Describe the company culture, some of the daily tasks, benefits and such. Please avoid copypasta :)"
+            }
+            name={`${personType}-message`}
+            onChange={handleChange}
+            rows={"10"}
+          />
+        </InputWrapper>
+      </>
+    )
+  }
+
+  const renderInquiryForm = () => {
+    return (
+      <>
+        <h3>Let's Do Something Cool!</h3>
+        <SliderWrapper>
+          <SliderLabel>Timeline</SliderLabel>
+          <Range
+            defaultValue={[3, 6]}
+            max={36}
+            tipFormatter={(value: string) => `${value} months`}
+            tipProps={{ visible: true }}
+            onChange={onTimelineChange}
+          />
+        </SliderWrapper>
+        <SliderWrapper>
+          <SliderLabel>Budget</SliderLabel>
+          <Range
+            allowCross={false}
+            min={5}
+            max={995}
+            defaultValue={[25, 100]}
+            tipFormatter={(value: string) => `$${value}k`}
+            tipProps={{ visible: true }}
+            step={10}
+            onChange={onBudgetChange}
+          />
+        </SliderWrapper>
+        <InputWrapper>
+          <label> What will we be doing?</label>
+          <StyledTextArea
+            placeholder={
+              "A nice introduction about you or your business and the project"
+            }
+            name={`${personType}-message`}
+            onChange={handleChange}
+            rows={"10"}
+          />
+        </InputWrapper>
+      </>
+    )
+  }
+
+  // About you
 
   return (
     <Layout location={props.location} title={"Contact"}>
-      <h3>You are: </h3>
-      <button onClick={() => setIsRecruiter(true)}>Recruiter</button>
-      <button onClick={() => setIsRecruiter(false)}>Entrepreneur</button>
+      <h3>About You</h3>
       <form
         name="contact"
         method="post"
@@ -96,38 +286,58 @@ export const Form = (props: Props) => {
         <p hidden={true}>
           <label>
             Don’t fill this out:{" "}
-            <input required={true} name="bot-field" onChange={handleChange} />
+            <input name="bot-field" onChange={handleChange} />
           </label>
         </p>
-
-        <InputWrapper>
-          <label>Name</label>
-          <StyledInput
-            required={true}
-            type="text"
-            name="name"
-            onChange={handleChange}
-          />
-        </InputWrapper>
-        <InputWrapper>
-          <label>Email</label>
-          <StyledInput
-            required={true}
-            type="email"
-            name="email"
-            onChange={handleChange}
-          />
-        </InputWrapper>
-        <p>
-          <label>
-            Description:
-            <br />
-            <textarea name="message" onChange={handleChange} />
-          </label>
-        </p>
-        <p>
-          <button type="submit">Send</button>
-        </p>
+        <BaseFormContainer>
+          <LeftBaseFormContainer>
+            <InputWrapper>
+              <label>Your Name</label>
+              <StyledInput
+                required={true}
+                type="text"
+                name="name"
+                onChange={handleChange}
+              />
+            </InputWrapper>
+            <InputWrapper>
+              <label>Email</label>
+              <StyledInput
+                required={true}
+                type="email"
+                name="email"
+                onChange={handleChange}
+              />
+            </InputWrapper>
+            <InputWrapper>
+              <label>Phone</label>
+              <StyledInput
+                required={true}
+                type="text"
+                name="phone"
+                onChange={handleChange}
+              />
+            </InputWrapper>
+          </LeftBaseFormContainer>
+          <RightBaseFormContainer>
+            <h4>Choose One</h4>
+            <AboutButtons>
+              <Button onClick={() => setIsPersonType("recruiter")}>
+                Recruiter
+              </Button>
+              <Button onClick={() => setIsPersonType("entrepreneur")}>
+                Entrepreneur
+              </Button>
+            </AboutButtons>
+          </RightBaseFormContainer>
+        </BaseFormContainer>
+        {personType === "recruiter" && renderJobForm()}
+        {personType === "entrepreneur" && renderInquiryForm()}
+        {personType && (
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        )}
       </form>
     </Layout>
   )
