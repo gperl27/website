e2b590eff39aefd72990c4b709bd3a194ea245b6
gperl27 <gperlman27@gmail.com>
2019-05-12 00:24:35 -0400

    production webpack build

--- webpack.production.js
+++ webpack.production.js
@@ -3,17 +3,11 @@ const TerserPlugin = require("terser-webpack-plugin")
 module.exports = {
   mode: "production",
   optimization: {
-    splitChunks: {
-      chunks: "all",
-    },
     minimizer: [new TerserPlugin({
       test: /\.[tj]sx?$/,
       terserOptions: {
-        compress: true,
-      },
+        compress: true
+      }
     })],
-  },
-  externals: {
-    nodegit: "commonjs nodegit",
   },
 }
