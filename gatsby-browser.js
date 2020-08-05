42ec58cd4718c5100ff59ba4905ef9115340846b
Greg <gperlman27@gmail.com>
2020-08-05 03:24:50 -0400

    Update gatsby-browser.js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,9 +5,17 @@ import "prismjs/themes/prism-tomorrow.css"
 
 import React from "react"
 import { ProviderWrapper } from "./src/contexts/ProviderWrapper"
+import { GlobalStyleWithTheme } from "./src/components/layout"
 
 export const wrapRootElement = ({ element }) => (
-  <ProviderWrapper>
-    {element}
-  </ProviderWrapper>
+  <ProviderWrapper>{element}</ProviderWrapper>
 )
+
+export const wrapPageElement = ({ element }) => {
+  return (
+    <>
+      <GlobalStyleWithTheme />
+      {element}
+    </>
+  )
+}
